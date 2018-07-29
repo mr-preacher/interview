abba657fd9cbe6398f5b882d04cd27a7482c56e8
mr-preacher <mr.preacher1981@gmail.com>
2018-07-29 15:37:36 -0400

    Add files via upload

---
+++ base64.py
@@ -0,0 +1,41 @@
+class Base64Transformer:
+    
+    dictionary = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','+','/']
+    
+    # 1 byte is 8 bit, base64 code is 6 bit
+    
+    def getSymbol(self, val):
+        return Base64Transformer.dictionary[val]
+    
+    def encode(self, arr):
+        result = []
+        
+        for j in range(0, len(arr)/3):
+            offset = 3*j;
+            result.append( self.getSymbol( ord(arr[offset])>>2 ) )
+            result.append( self.getSymbol( (ord(arr[offset]) & 3)<<4 | ord(arr[offset + 1])>>4 ) )
+            result.append( self.getSymbol( (ord(arr[offset + 1]) & 15)<<2 | ord(arr[offset + 2])>>6 ) )
+            result.append( self.getSymbol( ord(arr[offset + 2]) & 63 ) )
+        
+        offset = len(arr) - len(arr)%3;
+        if len(arr)%3 == 2:
+            result.append( self.getSymbol( ord(arr[offset])>>2) )
+            result.append( self.getSymbol( (ord(arr[offset]) & 3)<<4 | ord(arr[offset + 1])>>4 ) )
+            result.append( self.getSymbol( (ord(arr[offset + 1]) & 15)<<2) ) 
+            result.append("=")
+        elif len(arr)%3 == 1:
+            result.append( self.getSymbol( ord(arr[offset])>>2) )
+            result.append( self.getSymbol( (ord(arr[offset]) & 3)<<4) )
+            result.append("=")
+            result.append("=")
+        
+        return result
+            
+            
+
+o = Base64Transformer()
+
+print(o.encode(['M','a','n']))
+print(o.encode(['M','a']))
+print(o.encode(['M']))
